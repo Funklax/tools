9c69d5ffeda7f8119462f7a4d3e44b410cccb2d5
Kirk <captainkirk241@gmail.com>
2025-02-20 19:37:49 -0500

    Payload created

---
+++ TestPayload/Payload.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading;
+
+namespace Payload
+{
+    class Payload
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Payload working!");
+                Thread.Sleep(5000); // wait for 5 seconds
+            }
+        }
+    }
+}
